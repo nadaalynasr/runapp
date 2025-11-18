970a527c9d2d6d4ca35ed943325575225954e95e
Nada Aly-Nasr <nadaalynasr@Nadas-MacBook-Pro.local>
2025-11-18 10:47:12 -0500

    added navbar & functional files

---
+++ src/main/java/com/example/runapp/RunappApplication.java
@@ -0,0 +1,13 @@
+package com.example.runapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunappApplication.class, args);
+	}
+
+}
