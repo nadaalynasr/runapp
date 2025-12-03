644d4318c0794e15ec4416cb5bbd9f0a280cee21
JamieC003 <jamiechen914@gmail.com>
2025-12-03 18:59:51 -0500

    create group with name, date, time, member max working

--- src/main/java/com/example/runapp/controller/CreateGroupController.java
+++ src/main/java/com/example/runapp/controller/CreateGroupController.java
@@ -1,8 +1,11 @@
 package com.example.runapp.controller;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,32 +13,55 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.runapp.models.InputMetrics;
+import com.example.runapp.models.User;
+import com.example.runapp.services.CreateGroupService;
+import com.example.runapp.services.UserService;
 
 @Controller
 public class CreateGroupController {
+    
+    private final CreateGroupService createGroupService;
+    private final UserService userService;
+
+    @Autowired
+    public CreateGroupController(CreateGroupService createGroupService, UserService userService) {
+        this.createGroupService = createGroupService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create-group")
     public ModelAndView showCreateCroupPage() {
         ModelAndView mv = new ModelAndView("create_group");
         //centered title
-        mv.addObject("pageTitle", "NEW GROUP");
+        mv.addObject("pageTitle", "NEW GROUP RUN");
         //input title
-        mv.addObject("id", "create-group-title");
-        mv.addObject("name", "create_group_title");
+        mv.addObject("id", "create-group-name");
+        mv.addObject("name", "createGroupName");
         mv.addObject("placeholder", "Enter group name...");
         //input metrics
         List<InputMetrics> metrics = List.of(
-            new InputMetrics("day-id", "day", "ENTER DAY", "text"),
-            new InputMetrics("time-id", "time", "ENTER TIME", "text"),
-            new InputMetrics("max-id", "member_max", "MEMBER MAX", "number")
+            new InputMetrics("date-id", "date", "SCHEDULE DATE", "date"),
+            new InputMetrics("time-id", "time", "SCHEDULE TIME", "time"),
+            new InputMetrics("max-id", "memberMax", "MEMBER MAX", "number")
         );
          mv.addObject("metrics", metrics);
-         mv.addObject("buttonText", "POST GROUP");
+         mv.addObject("buttonText", "POST GROUP RUN");
 
         return mv;
     }
 
     @PostMapping("/create-group")
-    public String createGroup(@RequestParam String distance, @RequestParam String duration) {
-        return "redirect:/home";
+    public String createGroup(@RequestParam String createGroupName, 
+            @RequestParam LocalDate date,
+            @RequestParam LocalTime time,
+            @RequestParam int memberMax)
+            throws SQLException {
+
+        // String userId = userService.getLoggedInUser().getUserId();
+        String userId = "1"; // temp user id
+
+        createGroupService.createGroup(userId, createGroupName, date, time, memberMax);
+        
+        return "redirect:/groups";
     }
 }
