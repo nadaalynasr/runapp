825edddd31002faa574749b3075dd9975caaec9b
JamieC003 <jamiechen914@gmail.com>
2025-12-08 16:41:30 -0500

    updated my part of queries.sql

---
+++ queries.sql
@@ -0,0 +1,14 @@
+-- Creates a new group run
+-- This adds a new group run with necessary details to the database to be displayed on the groups page.
+-- http://localhost:8080/create-group
+insert into group_table (group_name, created_by_user_id, is_private, member_count, next_run_date, next_run_time, member_max) values (?, ?, ?, ?, ?, ?, ?);
+
+-- Adds creator to group
+-- This adds the creator of the group to the group they just created.
+-- http://localhost:8080/create-group
+insert into group_membership (user_id, group_id, joined_at, role) values (?, LAST_INSERT_ID(), NOW(), ?);
+
+-- Deletes a private group
+-- This deletes a private when the creater (only user) leaves the group.
+-- http://localhost:8080/groups
+delete from group_table where group_id = ? and is_private = true;
